18931b796d338140d40e5367db90254c6718ecf6
Chiro Liang <Chiro2001@163.com>
2024-04-15 22:43:18 +0800

    blocking spi works

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,31 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
+use embassy_executor::Spawner;
+use embassy_sync::blocking_mutex::NoopMutex;
 use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
 use esp_backtrace as _;
+use esp_hal::embassy;
 use esp_hal::spi::master::Spi;
 use esp_hal::spi::SpiMode;
+use esp_hal::timer::TimerGroup;
 use esp_hal::IO;
 use esp_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
 use esp_hal::{systimer::SystemTimer, Rng};
 use esp_wifi::{initialize, EspWifiInitFor};
 use log::*;
 
-struct SoftSpi<SCK, MOSI, CS, DELAY> {
-    sck: SCK,
-    mosi: MOSI,
-    cs: CS,
-    delay: DELAY,
-}
-#[derive(Debug)]
-enum SoftSpiError {
-    WriteError,
-}
-
-impl<SCK, MOSI, CS, DELAY> SoftSpi<SCK, MOSI, CS, DELAY>
-where
-    SCK: OutputPin,
-    MOSI: OutputPin,
-    CS: OutputPin,
-    DELAY: DelayNs,
-{
-    fn new(sck: SCK, mosi: MOSI, cs: CS, delay: DELAY) -> Self {
-        Self {
-            sck,
-            mosi,
-            cs,
-            delay,
-        }
-    }
-    fn write_u8(&mut self, data: u8) {
-        self.delay.delay_us(10);
-        for i in 0..8 {
-            if data & (1 << i) != 0 {
-                self.mosi.set_high().unwrap();
-            } else {
-                self.mosi.set_low().unwrap();
-            }
-            self.sck.set_high().unwrap();
-            self.delay.delay_us(10);
-            self.sck.set_low().unwrap();
-            self.delay.delay_us(10);
-        }
-        self.delay.delay_us(10);
-        self.mosi.set_high().unwrap();
-        // self.mosi.set_low().unwrap();
-    }
-    fn write(&mut self, data: &[u8]) -> Result<(), SoftSpiError> {
-        self.cs.set_low().unwrap();
-        self.delay.delay_us(20);
-        for d in data {
-            self.write_u8(*d);
-            self.delay.delay_us(20);
-        }
-        self.cs.set_high().unwrap();
-        self.delay.delay_us(20);
-        Ok(())
-    }
-    fn write_str(&mut self, data: &str) -> Result<(), SoftSpiError> {
-        let bytes = data.as_bytes();
-        let mut addr = 0x20;
-        for b in bytes {
-            self.write(&[addr, *b])?;
-            addr += 1;
-        }
-        Ok(())
-    }
-}
-
-#[entry]
-fn main() -> ! {
+#[main]
+async fn main(_spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-
     let clocks = ClockControl::max(system.clock_control).freeze();
+    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timg0);
+
     let mut delay = Delay::new(&clocks);
 
     esp_println::logger::init_logger_from_env();
@@ -100,81 +43,29 @@ fn main() -> ! {
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let sclk = io.pins.gpio2.into_push_pull_output();
     let mosi = io.pins.gpio3.into_push_pull_output();
-    let mut res = io.pins.gpio6.into_push_pull_output();
-    let mut en = io.pins.gpio5.into_push_pull_output();
+    let res = io.pins.gpio6.into_push_pull_output();
+    let en = io.pins.gpio5.into_push_pull_output();
     let cs = io.pins.gpio7.into_push_pull_output();
 
-    let mut spi = Spi::new(
+    let spi = Spi::new(
         peripherals.SPI2,
         fugit::HertzU32::kHz(100),
         SpiMode::Mode0,
         &clocks,
     )
     .with_sck(sclk)
-    .with_mosi(mosi)
-    .with_cs(cs)
-    ;
-    // let mut spi = SoftSpi::new(sclk, mosi, cs, delay);
+    .with_mosi(mosi);
 
-    OutputPin::set_high(&mut en).unwrap();
-
-    #[rustfmt::skip]
-    let init = [
-        [0xe0 as u8, 0x07], 
-        [0xe4, (32 - 1)], 
-        [0xe8, 0], 
-        [0xec, 0],
-        [0x20, 0x10],
-        [0x21, 0x10],
-        [0x22, 0x10],
-        [0x23, 0x10],
-        [0x24, 0x10],
-        [0x25, 0x10],
-        [0x26, 0x10],
-        [0x27, 0x10],
-    ];
-
+    let spi_mutex = NoopMutex::new(RefCell::new(spi));
+    let spi_device = SpiDevice::new(&spi_mutex, cs);
+    let mut buffer = [0u8; 8 * 5];
+    let mut vfd = vfd_8md06inkm::Vfd8md06inkm::new(spi_device, res, en, delay, &mut buffer);
+    vfd.init().unwrap();
+    // vfd.clear().unwrap();
     loop {
-        OutputPin::set_high(&mut res).unwrap();
-        DelayNs::delay_ms(&mut delay, 1);
-        OutputPin::set_low(&mut res).unwrap();
-        DelayNs::delay_ms(&mut delay, 10);
-        OutputPin::set_high(&mut res).unwrap();
-        DelayNs::delay_ms(&mut delay, 1);
-
-        for cmd in init.as_ref().iter() {
-            // spi.write(cmd).unwrap();
-            spi.write(&[reverse_bits(cmd[0]), reverse_bits(cmd[1])])
-                .unwrap();
-            DelayNs::delay_us(&mut delay, 40);
-        }
-
-        DelayNs::delay_ms(&mut delay, 10);
-
-        // spi.write_str("TEST").unwrap();
-        let s = b"TEST";
-        s.iter().enumerate().for_each(|(i, c)| {
-            // spi.write(&[0x20 + i as u8, *c]).unwrap();
-            spi.write(&[reverse_bits(0x20 + i as u8), reverse_bits(*c)])
-                .unwrap();
-            DelayNs::delay_us(&mut delay, 40);
-        });
-
+        // vfd.init().unwrap();
+        // vfd.clear().unwrap();
+        vfd.write_str(0, "TEST").unwrap();
         DelayNs::delay_ms(&mut delay, 1000);
     }
-}
-
-// const fn reverse_bits(x: u8) -> u8 {
-//     x
-// }
-
-const fn reverse_bits(x: u8) -> u8 {
-    ((x & 0b00000001) << 7)
-        | ((x & 0b00000010) << 5)
-        | ((x & 0b00000100) << 3)
-        | ((x & 0b00001000) << 1)
-        | ((x & 0b00010000) >> 1)
-        | ((x & 0b00100000) >> 3)
-        | ((x & 0b01000000) >> 5)
-        | ((x & 0b10000000) >> 7)
 }
---
+++ vfd-8md06inkm/src/lib.rs
@@ -0,0 +1,131 @@
+#![no_std]
+
+use embedded_hal::spi::Operation;
+
+#[repr(u8)]
+pub enum Commands {
+    DCramDataWrite = 0x20,
+    DGramDataClear = 0x10,
+    CGramDataWrite = 0x40,
+    SetDisplayTiming = 0xe0,
+    SetDimmingData = 0xe4,
+    SetDisplayOn = 0xe8,
+    SetDisplayOff = 0xea,
+    SetStandByMode = 0xec,
+}
+impl Into<u8> for Commands {
+    fn into(self) -> u8 {
+        self as u8
+    }
+}
+
+const fn reverse_bits(x: u8) -> u8 {
+    ((x & 0b00000001) << 7)
+        | ((x & 0b00000010) << 5)
+        | ((x & 0b00000100) << 3)
+        | ((x & 0b00001000) << 1)
+        | ((x & 0b00010000) >> 1)
+        | ((x & 0b00100000) >> 3)
+        | ((x & 0b01000000) >> 5)
+        | ((x & 0b10000000) >> 7)
+}
+
+pub type Result<T> = core::result::Result<T, display_interface::DisplayError>;
+
+pub struct Vfd8md06inkm<'d, SPI, R, E, D> {
+    spi: SPI,
+    reset: R,
+    enabled: E,
+    delay: D,
+    buffer: &'d mut [u8],
+}
+
+impl<'d, SPI, R, E, D> Vfd8md06inkm<'d, SPI, R, E, D>
+where
+    SPI: embedded_hal::spi::SpiDevice,
+    R: embedded_hal::digital::OutputPin,
+    E: embedded_hal::digital::OutputPin,
+    D: embedded_hal::delay::DelayNs,
+{
+    pub fn new(spi: SPI, reset: R, enabled: E, delay: D, buffer: &'d mut [u8]) -> Self {
+        Self {
+            spi,
+            reset,
+            enabled,
+            delay,
+            buffer,
+        }
+    }
+    fn trans_delay() -> u32 {
+        // 40_000
+        0
+    }
+    pub fn digits(&self) -> usize {
+        self.buffer.len() / 5
+    }
+    pub fn write_reg<C>(&mut self, cmd: C, data: u8) -> Result<()>
+    where
+        C: Into<u8>,
+    {
+        self.spi
+            .transaction(&mut [
+                Operation::Write(&[reverse_bits(cmd.into()), reverse_bits(data)]),
+                Operation::DelayNs(Self::trans_delay()),
+            ])
+            .map_err(|_| display_interface::DisplayError::BusWriteError)?;
+        Ok(())
+    }
+    pub fn write_data<C>(&mut self, cmd: C, data: &[u8]) -> Result<()>
+    where
+        C: Into<u8>,
+    {
+        let mut buf = [0u8; 16];
+        buf.iter_mut()
+            .zip([cmd.into()].iter().chain(data.iter()))
+            .for_each(|(b, d)| {
+                *b = reverse_bits(*d);
+            });
+        self.spi
+            .transaction(&mut [
+                Operation::Write(&buf[..data.len() + 1]),
+                Operation::DelayNs(Self::trans_delay()),
+            ])
+            .map_err(|_| display_interface::DisplayError::BusWriteError)?;
+        Ok(())
+    }
+    pub fn init(&mut self) -> Result<()> {
+        self.enabled
+            .set_high()
+            .map_err(|_| display_interface::DisplayError::RSError)?;
+        self.reset
+            .set_low()
+            .map_err(|_| display_interface::DisplayError::RSError)?;
+        self.delay.delay_ms(10);
+        self.reset
+            .set_high()
+            .map_err(|_| display_interface::DisplayError::RSError)?;
+        self.delay.delay_ms(1);
+
+        use Commands::*;
+        self.write_reg(SetDisplayTiming, self.digits() as u8 - 1)?;
+        self.write_reg(SetDimmingData, 31)?;
+        self.write_reg(SetDisplayOn, 0)?;
+        Ok(())
+    }
+    pub fn clear(&mut self) -> Result<()> {
+        for d in 0..self.digits() {
+            let addr = d as u8 + Commands::DCramDataWrite as u8;
+            self.write_reg(addr, Commands::DGramDataClear.into())?;
+        }
+        Ok(())
+    }
+    pub fn write_str(&mut self, start: u8, s: &str) -> Result<()> {
+        if start > self.digits() as u8 {
+            return Err(display_interface::DisplayError::OutOfBoundsError);
+        }
+        let s = &s.as_bytes()[..(s.len().min(self.digits() - start as usize))];
+        let addr = start + Commands::DCramDataWrite as u8;
+        self.write_data(addr, s)?;
+        Ok(())
+    }
+}
